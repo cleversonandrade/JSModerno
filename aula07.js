7977c0dfdffc3a60063ce7c6a009e9bb90108eed
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-01 19:08:32 -0300

    aula07_parte1

---
+++ aula07.js
@@ -0,0 +1,12 @@
+// && -> and e
+// || -> or ou
+// ! -> not não
+
+let n1 = 10;
+let n2 = 5;
+let n3 = 15;
+let n4 = 2;
+
+console.log((n1 > n2)&&(n1 > n3));
+console.log((n2 < n1)&&(n4 > n2));
+console.log((n1 > n4)&&(n3 > n2));

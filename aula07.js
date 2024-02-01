7c408afefc86c41711f1e3e8ee578dc13a2db8c6
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-01 19:37:25 -0300

    aula07_parte3

--- aula07.js
+++ aula07.js
@@ -7,6 +7,8 @@ let n2 = 5;
 let n3 = 15;
 let n4 = 2;
 
-console.log((n1 > n2)||(n1 > n3));
-console.log((n2 < n1)||(n4 > n2));
-console.log((n1 > n4)||(n3 > n2));
+if (!(n1 > n2) && (n3 > n4)) {
+    console.log('Verdadeiro')
+} else {
+    console.log('Falso')
+}

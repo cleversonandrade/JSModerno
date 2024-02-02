0da98edd7ee50d488ae48a37319b38a9f6fad7dc
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-02 18:13:10 -0300

    aula10

---
+++ aula10.js
@@ -0,0 +1,12 @@
+let estado = 'A';
+
+
+let res = (estado == 'A' ?'Ativo':'Inativo');
+
+// if(res == 0) {
+//     console.log('Par')
+// } else {
+//     console.log('Ímpar')
+// }
+
+console.log(res);

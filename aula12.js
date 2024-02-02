529bb6dc5ca72508dc45f8e395649dc2bc2c0f9f
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-02 19:11:17 -0300

    aula12_parte2

--- aula12.js
+++ aula12.js
@@ -1,5 +1,7 @@
-let jogador1 = {nome: 'Cleverson', energia: 100, vidas: 3, magia: 150};
-let jogador2 = {nome: 'Pietro', energia: 100, vidas: 5, velocidade: 200};
-let jogador3 = {...jogador1,...jogador2};
+const soma = (v1,v2,v3) => {
+    return v1 + v2 + v3
+}
 
-console.log(jogador3);
+let valores = [2,3,5];
+
+console.log(soma(...valores));

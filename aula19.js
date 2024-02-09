1666a63655ec3cb096f4c38b755761ea73da6b70
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-09 20:48:36 -0300

    aula19_parte3

--- aula19.js
+++ aula19.js
@@ -1,12 +1,13 @@
 let n = 0;
 let max = 1000;
+let pares = 0;
 
-while(n < max) {
-    console.log('Cleverson - ' + n);
-    if(n >= 10) {
-        break;
+for(let i = n; i < max; i++) {
+    if(i%2 != 0) {
+        continue;
     }
-    n++;
+    pares++;
 }
 
+console.log('Quantidade de pares: ' + pares);
 console.log('Fim do programa');

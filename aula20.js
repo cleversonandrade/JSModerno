7867bc070465baf98ce7b0be609d4c7a3ebdfa16
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-10 20:13:21 -0300

    aula20_parte2

--- aula20.js
+++ aula20.js
@@ -1,10 +1,8 @@
-function soma() {
-    let n1 = 10;
-    let n2 = 5;
-    let soma = n1 + n2
-    console.log(soma)
-}
-
-for(let i = 0; i < 10; i++) {
-    soma()
+function mudarTexto() {
+    let d1 = document.getElementById('d1');
+    let d2 = document.getElementById('d2');
+    let d3 = document.getElementById('d3');
+    d1.innerHTML = 'Texto manipulado 1';
+    d2.innerHTML = 'Texto manipulado 2';
+    d3.innerHTML = 'Texto manipulado 3';
 }

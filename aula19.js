2656f64f96576e4b002928f1dacf59d9f379a5f6
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-10 19:36:56 -0300

    aula20_parte1

--- aula19.js
+++ aula19.js
@@ -2,8 +2,8 @@ let n = 0;
 let max = 1000;
 let pares = 0;
 
-for(let i = n; i < max; i++) {
-    if(i%2 != 0) {
+for(let i = 0; i < max; i++) {
+    if(i%2 == 0) {
         continue;
     }
     pares++;
---
+++ aula20.js
@@ -0,0 +1,10 @@
+function soma() {
+    let n1 = 10;
+    let n2 = 5;
+    let soma = n1 + n2
+    console.log(soma)
+}
+
+for(let i = 0; i < 10; i++) {
+    soma()
+}

2c7f434439a73082c40ca18e2f81034a3db82974
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-10 21:42:57 -0300

    aula22_parte6

--- aula22.js
+++ aula22.js
@@ -1,8 +1,24 @@
-function soma(n1 = 5, n2 = 3) {
-    let res 
+const valorPadrao = 0;
+
+let valor = 0;
+
+console.log(valor);
+
+function add(v) {
+    valor += v
+}
+
+add(5);
+console.log(valor);
+
+add(10);
+console.log(valor);
+
+function somar(n1 = valorPadrao, n2 = valorPadrao) {
+    let res;
     res = n1 + n2
     return res
 }
 
-let resultado = soma(5,5)
-console.log(resultado);
+let resultado = somar(5,7);
+//console.log(resultado);

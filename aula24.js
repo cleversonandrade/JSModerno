2d05905e3cc0eac2e2efd92fdded219220013065
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-12 19:52:09 -0300

    aula24_parte3

--- aula24.js
+++ aula24.js
@@ -1,9 +1,3 @@
-const f = function(...valores) {
-    let res = 0;
-    for(let v of valores) {
-        res += v;
-    }
-    return res;
-}
+const f = new Function('v1', 'v2', 'return v1 + v2')
 
-console.log(f(10, 10));
+console.log(f(6, 6));

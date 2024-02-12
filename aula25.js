711b90c018a1d00e5b9a78d6a510bb307552a8bc
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-12 20:41:00 -0300

    aula25_parte2

--- aula25.js
+++ aula25.js
@@ -1,11 +1,11 @@
-const soma = (n1, n2) =>  n1 * n2;
-
-console.log(soma(4, 3));
-
+const soma = (n1, n2) => {
+    let res = n1 * n2;
+    return res
+} 
+console.log(soma(4, 5));
 
 const add = n => n + 10;
 console.log(add(5))
-
 
 const nome = n =>  n;
 console.log(nome('Cleverson'));

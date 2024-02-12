4160729ff1cb8ae914c18fe513f0130c98ee3503
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-12 19:18:16 -0300

    aula23_parte3

--- aula23.js
+++ aula23.js
@@ -1,10 +1,9 @@
 function soma(...valores) {
-    let tam = valores.length;
     let res = 0;
-    for(let i = 0; i < tam; i++) {
-        res+=valores[i]
+    for(let v of valores) {
+        res += v;
     }
     return res;
 }
 
-console.log(soma(10, 5));
+console.log(soma(2, 3, 5, 5))

f2c78c4098225a8f5dd7c7f061bf26cab8f16eb9
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-14 19:58:14 -0300

    aula27_parte4

--- aula27.js
+++ aula27.js
@@ -2,11 +2,13 @@ function* contador() {
     let i = 0;
     while(true) {
         yield i++
+        if(i > 5) 
+            break;
+        
     }
 }
 
 const itc = contador();
-for(let i = 0; i <= 15; i++) {
-    console.log(itc.next().value);
+for(let c of itc) {
+    console.log(c)
 }
-

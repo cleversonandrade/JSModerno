fc337fce14407383f3e8dbb810104ea8a70d137c
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-15 20:51:54 -0300

    aula28_parte3

--- aula28.js
+++ aula28.js
@@ -1,6 +1,12 @@
-const elementos = document.getElementsByTagName('div');
-const valores = Array.prototype.map.call(elementos, ({innerHTML}) => innerHTML)
-console.log(valores);
+const converterInt = (e) => parseInt(e);
+const dobrar = (e) => e*2
+let num = ['1', '2', '3', '4', '5'].map(dobrar);
+console.log(num);
+
+
+// const elementos = document.getElementsByTagName('div');
+// const valores = Array.prototype.map.call(elementos, ({innerHTML}) => innerHTML)
+// console.log(valores);
 
 // let elementos = document.getElementsByTagName('div');
 // elementos = [...elementos];

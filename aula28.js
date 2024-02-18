0a2917c6e2a661e0177012b1519f4d6efbaef449
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-18 11:48:58 -0300

    aula29_parte1

--- aula28.js
+++ aula28.js
@@ -1,26 +1,8 @@
 const converterInt = (e) => parseInt(e);
-const dobrar = (e) => e*2
-let num = ['1', '2', '3', '4', '5'].map(dobrar);
+const dobrar = (e) => e*2;
+const num = ['1', '2', '3', '4', '5'].map(dobrar);
 console.log(num);
 
-
 // const elementos = document.getElementsByTagName('div');
-// const valores = Array.prototype.map.call(elementos, ({innerHTML}) => innerHTML)
+// const valores = Array.prototype.map.call(elementos, ({innerHTML}) => innerHTML);
 // console.log(valores);
-
-// let elementos = document.getElementsByTagName('div');
-// elementos = [...elementos];
-// console.log(elementos);
-// elementos.map((e, i) => {
-//     e.innerHTML = 'Texto alterado'
-// });
-
-
-
-
-// const cursos = ['HTML', 'CSS', 'JavaScript', 'PHP', 'REACT'];
-// let c = cursos.map((e, i) => {
-    //     return e 
-    // })
-    
-    // console.log(c)

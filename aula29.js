1f3915c294889ae0c53d5b5923ef659a07da9428
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-18 12:09:32 -0300

    aula29_parte2

--- aula29.js
+++ aula29.js
@@ -2,8 +2,21 @@ function aluno(nome, nota) {
     this.nome = nome;
     this.nota = nota;
 
-    console.log(nome);
-    console.log(nota);
+    this.dados = function() {
+        setTimeout(function(){
+            console.log(this.nome);
+            console.log(this.nota)
+        }, 2000)
+    }
+
+    this.dados1 = function() {
+        setTimeout(() => {
+            console.log(this.nome);
+            console.log(this.nota)
+        }, 2000)
+    }
 }
 
-aluno('Cleverson', 95);
+const aluno1 = new aluno('Cleverson', 95);
+aluno1.dados();
+aluno1.dados1();

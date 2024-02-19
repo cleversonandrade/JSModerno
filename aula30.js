9bef003e053c421d6a198b0583bbc01771b6ef02
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-19 20:00:18 -0300

    aula31_parte1

--- aula30.js
+++ aula30.js
@@ -5,15 +5,11 @@ const elemento4 = document.getElementById('c4');
 const elemento5 = document.getElementById('c5');
 const elemento6 = document.getElementById('c6');
 
-const arrayElementos = [elemento1, elemento2, elemento3, elemento4, elemento5, elemento6];
+const arrayElementos = [elemento1, elemento2, elemento3, 
+                        elemento4, elemento5, elemento6];
 
-// for(e of arrayElementos) {
-//     e.innerHTML = 'Cleverson';
-// }
 
 arrayElementos.map((e) => {
     e.innerHTML = 'Cleverson';
     console.log(e);
-})
-
-// console.log(arrayElementos);
+});

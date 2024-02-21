dd8cc47f6b9d0c4dda8b58170353e83cfdd0af8d
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-20 21:04:55 -0300

    aula33

--- aula32.js
+++ aula32.js
@@ -1,8 +1,7 @@
 const todosCursos = [...document.getElementsByClassName('curso')];
 const todosCursosC1 = [...document.getElementsByClassName('c1')];
 const todosCursosC2 = [...document.getElementsByClassName('c2')];
-const cursoEspecial = document.getElementsByClassName('curso')[0];
-
+const cursoEspecial = [...document.getElementsByClassName('curso')][1];
 
 console.log(todosCursos);
 console.log(todosCursosC1);

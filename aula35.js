86d35eeeebc7fa44bab448e6cc5f742bd367e3a0
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-23 19:36:04 -0300

    aula35_36

--- aula35.js
+++ aula35.js
@@ -1,6 +1,6 @@
 const caixa1 = document.querySelector('#caixa1');
 const caixa2 = document.querySelector('#caixa2');
-const botao = document.querySelector('#botao_copiar');
+const botaoTransferir = document.querySelector('#botao_transferir');
 const cursos = [...document.querySelectorAll('.curso')];
 
 cursos.map((elemento) => {
@@ -11,9 +11,16 @@ cursos.map((elemento) => {
     });
 });
 
-botao.addEventListener('click', () => {
+botaoTransferir.addEventListener('click', () => {
     const cursosSelecionados = [...document.querySelectorAll('.selecionado')];
+    const cursosNaoSelecionados = [...document.querySelectorAll('.curso:not(.selecionado)')];
+
     cursosSelecionados.map((elemento) => {
         caixa2.appendChild(elemento);
     });
+
+    cursosNaoSelecionados.map((elemento) => {
+        caixa1.appendChild(elemento);
+    });
 });
+

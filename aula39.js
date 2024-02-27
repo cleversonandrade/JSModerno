c6225af5a6b72b65dc7ac2c2d6338eba1bb9252e
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-02-27 20:03:14 -0300

    aula39

---
+++ aula39.js
@@ -0,0 +1,18 @@
+const caixa1 = document.querySelector('#caixa1');
+const btn_c = [...document.querySelectorAll('.curso')];
+
+const c1_2 = document.querySelector('#c1_2');
+
+console.log(c1_2.parentNode.parentNode.children[4]);
+
+
+// console.log(caixa1.hasChildNodes());
+// console.log(btn_c[0].hasChildNodes());
+// console.log(btn_c[0].childNodes);
+
+// console.log(btn_c[0].children.length > 0 ? 'Possui filhos' : 'Não possui filhos');
+
+// console.log(caixa1.children[2].innerHTML = 'Jéssica')
+// caixa1.firstElementChild.innerHTML = 'Cleverson'
+// caixa1.children[1].innerHTML = 'Kauã';
+// caixa1.children[3].innerHTML = 'Pietro';

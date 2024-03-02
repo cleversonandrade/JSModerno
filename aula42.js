d7b2ba521eb9ace2d4156b2b0567b53a6f0b802e
Cleverson Augusto de Andrade <cleversonandrade62@gmail.com>
2024-03-02 19:06:27 -0300

    aula42_parte2

--- aula42.js
+++ aula42.js
@@ -1,12 +1,16 @@
-const filtroMaior18 = (valor) => {
-    if(valor >= 18) {
-        return valor;
-    };
-};
-
 const idades = [15, 21, 30, 17, 18, 44, 12, 50];
-const maior = idades.filter(filtroMaior18);
+const maior = idades.filter((val, ind, arr) => {
+    if(val >= 18) {
+        return val
+    };
+});
 
+const menorIdade = idades.filter((v, i, a) => {
+    if(v <= 18) {
+        return v;
+    }
+})
 
-console.log(idades);
-console.log(maior);
+console.log(idades)
+console.log(maior)
+console.log(menorIdade)
